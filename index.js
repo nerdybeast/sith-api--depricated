7ea1516badb30254f0b1a962297ef0a01ad62d18
Michael Penrod <michael.penrod@vivint.com>
2016-02-11 22:44:23 -0700

    Adding some core npm packages.

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
+//default node.js modules
+var http = require('http');
+
+//npm installed packages
 var express = require('express');
+var morgan = require('morgan');
+//var debug = require('debug')('SITH-API'); //not working
+var cookieParser = require('cookie-parser'); //middle-ware that allows easy manipulation of cookies incoming/outgoing
+var bodyParser = require('body-parser'); //middle-ware that allows easy manipulation of request/response bodies
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
+//debug(app.get('port'));
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(__dirname + '/public'));
 
