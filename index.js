8f1bea9aeb967d2a07f0e16f05848e1641f23074
Michael Penrod <michael.penrod@vivint.com>
2016-02-17 20:27:31 -0700

    Removed static files to make this truly a json api. Bumped node version to 5.6.0

--- index.js
+++ index.js
@@ -4,31 +4,35 @@ var http = require('http');
 //npm installed packages
 var express = require('express');
 var morgan = require('morgan');
-//var debug = require('debug')('SITH-API'); //not working
 var cookieParser = require('cookie-parser'); //middle-ware that allows easy manipulation of cookies incoming/outgoing
 var bodyParser = require('body-parser'); //middle-ware that allows easy manipulation of request/response bodies
 
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-//debug(app.get('port'));
+app.set('port', (process.env.PORT || 4000));
 
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function(req, res) {
+    res.send({
+        port: app.get('port'),
+        env: process.env.NODE_ENV
+    });
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+var server = http.createServer(app);
+
+server.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
+
+server.on('error', function(err) {
+    console.log('Server error =>', err);
+});
+
+server.on('listening', function() {
+    console.log('Server listening');
 });
