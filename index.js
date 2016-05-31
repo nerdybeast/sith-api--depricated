f733c3fff51f6cf051c07bd9b3c95608067fef67
nerdybeast <therealnerdybeast@gmail.com>
2016-05-31 17:28:58 -0600

    Fixed property casing for use with socket.io

--- index.js
+++ index.js
@@ -12,45 +12,50 @@ var cors = require('cors');
 
 var app = express();
 
-//app.set('port', (process.env.PORT || 5000));
-let server = app.listen((process.env.PORT || 5000));
+//Storing some custom settings on the Express object so that they can be accessed later if neccessary.
+//NOTE: See http://expressjs.com/en/4x/api.html#app.settings.table for built-in Express settings. 
+app.set('port', (process.env.PORT || 5000));
+
+let server = app.listen(app.get('port'));
 let io = require('socket.io')(server);
 
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+//Currently this is used to enable all cross origin requests.
+//TODO: Restrict access when in production, possibly staging as well...
 app.use(cors());
-
-// //Automatically loads routes/index.js, see:
-// //https://nodejs.org/api/modules.html#modules_folders_as_modules
-// require('./routes')(app);
-
-//var server = http.createServer(app);
 
 //Automatically loads routes/index.js, see:
 //https://nodejs.org/api/modules.html#modules_folders_as_modules
 require('./routes')(app, io);
 
-//server.listen(app.get('port'));
+server.on('listening', function() {
+    console.log('Node app is running on port', app.get('port'));
+});
 
 server.on('error', function(err) {
     console.log('Server error =>', err);
 });
 
-server.on('listening', function() {
-    console.log('Node app is running on port', app.get('port'));
-});
+
+
+// io.use((socket, next) => {
+//     console.log(socket.request);
+//     next();
+// });
 
 io.on('connection', (socket) => {
     
     console.log('Client Connected');
     
     //Will emit to just the new client that has connected.
-    socket.emit('debug-from-server', { message: 'Welcome new client!' });
+    socket.emit('debug-from-server', 'Welcome new client!');
     
     //Will emit to all clients that are connected.
-    io.emit('debug-from-server', { message: 'Hey everyone, there is a new client!' });
+    io.emit('debug-from-server', 'Hey everyone, there is a new client!');
     
     //Custom hook
     socket.on('debug-from-client', function(from, data) {
