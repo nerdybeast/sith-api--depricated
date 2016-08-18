af508c4c2e841a8baf77a76013291a1564c3775c
Michael Penrod <therealnerdybeast@gmail.com>
2016-08-17 22:40:42 -0600

    Removing unneeded route.

--- routes/api/manage-users.js
+++ routes/api/manage-users.js
@@ -11,21 +11,6 @@ const db = require('../../lib/db');
 let _debug = new Debug('MANAGE-USERS');
 let router = express.Router();
 
-router.route('/').post((req, res, next) => {
-    
-    //Will be the new user's profile object after having just authenticated with Salesforce, ex:
-    //{ active: true, organization_id: '00D17000000BLCaEAO', id: 'https://test.salesforce.com/id/00D16000000ALCaEAO/005F0000003pgl8IAA', etc... }
-    let profile = req.body;
-
-    db.insertProfile(profile).then(result => {
-        return res.send();
-    }).catch(error => {
-        let exception = new Error(error);
-        return next(exception);
-    });
-
-}).all(routeErrorHandler);
-
 router.route('/:user_id').get((req, res, next) => {
 
     let profile;
