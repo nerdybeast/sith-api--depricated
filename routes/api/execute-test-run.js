d4a85c5eaa948770a5a216a0acf1e35bb91965fb
Michael Penrod <therealnerdybeast@gmail.com>
2016-09-21 17:50:46 -0600

    Started building out the new Salesforce class and converting various endpoints to use it.

--- routes/api/execute-test-run.js
+++ routes/api/execute-test-run.js
@@ -11,8 +11,6 @@ let Debug = require('../../lib/debug');
 let _debug = new Debug('EXECUTE_TEST_RUN');
 
 let router = express.Router();
-let jExt;
-let io;
 
 router.route('/').post(function(req, res, next) {
     
@@ -20,12 +18,12 @@ router.route('/').post(function(req, res, next) {
     let userId = req.body.data.userId;
     let acceptsJsonApi = req.headers.acceptsJsonApi;
     let jExt = req.app.get('jExt');
+    let sf = req.app.get('sf');
     
     //TODO: need to see if creating a new traceflag here is going to interupt a test that is currently running.
     //If so, we will need to check for that before creating the new traceflag.
-    return jExt.createAnalyticsTraceFlag(userId).then(result => {
+    return sf.createAnalyticsTraceFlag(userId).then(result => {
     
-        //io.emit('debug-from-server', { traceFlag: result });
         _debug.log('Created TraceFlag', result);
     
         return jExt.triggerAsyncTestRun(classIds);
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const jwt = require('express-jwt');
 const debug = require('debug')('api index');
 const rollbar = require('rollbar');
 const JsforceExt = require('../lib/jsforceExt');
+const contentNegotiation = require('../lib/content-negotiation');
+const Salesforce = require('../lib/salesforce');
 
 rollbar.init(process.env.ROLLBAR_ACCESS_TOKEN);
 
@@ -24,44 +26,8 @@ let RoutesCore = function(app) {
         });
     });
     
-    app.use(function(req, res, next) {
-        
-        const jsonMimeType = 'application/json';
-        const jsonApiMimeType = 'application/vnd.api+json';
-        
-        //These variables will return a string if the "Accept" header contains one of the above supported media types
-        //otherwise, they will return false.
-        let acceptsJson = req.accepts(jsonMimeType);
-        let acceptsJsonApi = req.accepts(jsonApiMimeType);
-        
-        //These variables will also return a string if the given value matches the Content-Type of the request, here we are forcing into a boolean value.
-        let isPlainJson = (req.is(jsonMimeType) !== false);
-        let isJsonApi = (req.is(jsonApiMimeType) !== false);
-
-        if(!acceptsJson && !acceptsJsonApi) {
-            let err = new Error(`All requests to this api must specify an "Accept" header with a value of either "${jsonApiMimeType}" or "${jsonMimeType}".`);
-            err.type = 'UNSUPPORTED_MEDIA_TYPE';
-            err.statusCode = 415;
-            return next(err);
-        }
-
-        //NOTE: Content-Type is not usually passed with some http verbs such as a GET request.
-        if(req.get('Content-Type') && !isPlainJson && !isJsonApi) {
-            let err = new Error(`All requests to this api containing a body (valid json), must specify a "Content-Type" header with a value of either "${jsonApiMimeType}" or "${jsonMimeType}".`);
-            err.type = 'UNSUPPORTED_MEDIA_TYPE';
-            err.statusCode = 415;
-            return next(err);
-        }
-        
-        //Let's see if the incoming request is asking for a json api doc to be returned. Tacking on an additional
-        //header so that we can determine what type of content to return.
-        //See: http://expressjs.com/en/api.html#req.accepts
-        req.headers.acceptsJsonApi = (acceptsJsonApi && acceptsJsonApi !== false);
-        
-        req.headers.isJsonApi = isJsonApi;
-
-        return next();
-    });
+    //Restricts the type of data that can be sent to this api.
+    app.use(contentNegotiation);
     
     app.use('/analytics', require('./analytics'));
     app.use('/services', require('./org-services'));
@@ -87,6 +53,7 @@ let RoutesCore = function(app) {
         }; 
 
         app.set('jExt', new JsforceExt(connectionDetails, profile, io));
+        app.set('sf', new Salesforce(connectionDetails, profile));
 
         //Turn this query param into a solid boolean
         req.query.force = (req.query.force !== undefined);
