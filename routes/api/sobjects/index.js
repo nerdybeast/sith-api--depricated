6ce5a7997cf35be97bbe93bc0a0159dc2ed93107
Michael Penrod <therealnerdybeast@gmail.com>
2016-12-01 22:12:53 -0700

    Started work on describing sobjects for usage in the UI.

--- routes/api/sobjects/index.js
+++ routes/api/sobjects/index.js
@@ -1,8 +1,47 @@
 'use strict';
 
 const express = require('express');
+const customSerializer = require('../../../lib/json-api-serializer');
+const _ = require('lodash');
 
 let router = express.Router();
+
+router.route('/').get(function(req, res, next) {
+
+    let sf = req.app.get('sf');
+
+    sf.globalDescribe().then(data => {
+        let jsonApiDoc = customSerializer.baseSerializer('sobject', data.fieldNames, data.sobjects);
+        return res.send(jsonApiDoc);
+    });
+
+});
+
+router.route('/describe/:sobject').get(function(req, res, next) {
+
+    let sf = req.app.get('sf');
+    let sobjectName = req.params.sobject;
+
+    sf.describeSobject(sobjectName).then(data => {
+
+        let fieldDescriptionFieldNames = _.keys(data.result.fields[0]);
+
+        //Give each field description a unique id that associates it with the given sobject.
+        data.result.fields = data.result.fields.map(field => {
+            field.id = `${sobjectName}:${field.name}`;
+        });
+
+        let fieldDescriptionDocs = customSerializer.baseSerializer('field-description', fieldDescriptionFieldNames, data.result.fields);
+
+        data.result.fields = data.result.fields.map(field => {
+            return { type: 'field-description', id: field.id };
+        });
+
+        
+
+    });
+
+});
 
 router.use('/trace-flags', require('./trace-flags'));
 router.use('/debug-levels', require('./debug-levels'));
